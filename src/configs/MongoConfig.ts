884db8a9705ee25d9ce7b3bd3ffdfc4d5bae3e64
Mert <mertturkmenoglu99@gmail.com>
2021-03-29 19:00:55 +0300

    Add MongoDB config

---
+++ src/configs/MongoConfig.ts
@@ -0,0 +1,9 @@
+import { ConnectOptions } from 'mongoose';
+
+const mongooseOptions: ConnectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
+
+export default mongooseOptions;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import dotenvSafe from 'dotenv-safe';
+import mongoose from 'mongoose';
+
+import mongooseOptions from './configs/MongoConfig';
 
 dotenvSafe.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI as string, mongooseOptions, () => {
+  console.log('Connected to MongoDB');
+});
 
 app.listen(5000, () => {
   console.log('Server started on port 5000');
