6660acaf240a8c0389b56451ee2288bff232e199
Mert <mertturkmenoglu99@gmail.com>
2021-03-30 10:39:42 +0300

    Fix register dto password validation

--- src/validation/register.ts
+++ src/validation/register.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 const registerSchema = yup.object().shape({
   username: yup.string().required().max(32).lowercase(),
   email: yup.string().required().email().max(255),
-  password: yup.string().required().min(8).lowercase(),
+  password: yup.string().required().min(8),
   name: yup.string().required().max(255).trim(),
 });
 
