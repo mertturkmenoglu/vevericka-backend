b1cf04923c4554503e318358cabe9060a13e6d9b
Mert <mertturkmenoglu99@gmail.com>
2021-03-30 14:56:01 +0300

    Update isAuth logic

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
 import err from '../utils/err';
+import getToken from '../utils/getToken';
 import HttpCodes from '../utils/HttpCodes';
 
 const isAuth = (req: Request, res: Response, next: NextFunction) => {
@@ -9,16 +10,17 @@ const isAuth = (req: Request, res: Response, next: NextFunction) => {
     return res.status(HttpCodes.UNAUTHORIZED).json(err('Not authorized', HttpCodes.UNAUTHORIZED));
   }
 
-  const header = req.headers.authorization;
-  const headerParts = header.toString().split(' ');
+  const header = req.headers.authorization.toString();
+  const token = getToken(header);
 
-  if (headerParts.length === 2 || headerParts[0] === 'Bearer') {
-    const token = headerParts[1];
-    const verified = jwt.verify(token, process.env.JWT_SECRET as string);
+  if (!token) {
+    return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authotization token', HttpCodes.UNAUTHORIZED));
+  }
 
-    if (!verified) {
-      return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authotization token', HttpCodes.UNAUTHORIZED));
-    }
+  const verified = jwt.verify(token, process.env.JWT_SECRET as string);
+
+  if (!verified) {
+    return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authotization token', HttpCodes.UNAUTHORIZED));
   }
 
   return next();
