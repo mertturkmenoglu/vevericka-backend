740a4cc320032d10d62e980ae608311bdb29be4f
Mert <mertturkmenoglu99@gmail.com>
2021-04-10 22:39:50 +0300

    Fix typo in isAuth.ts

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -10,19 +10,18 @@ const isAuth = (req: Request, res: Response, next: NextFunction) => {
     const token = getTokenFromHeader(req);
 
     if (!token) {
-      return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authotization token', HttpCodes.UNAUTHORIZED));
+      return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authorization token', HttpCodes.UNAUTHORIZED));
     }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET as string);
 
     if (!verified) {
-      return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authotization token', HttpCodes.UNAUTHORIZED));
+      return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authorization token', HttpCodes.UNAUTHORIZED));
     }
 
     return next();
   } catch (e) {
-    console.log(e.message);
-    return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authotization token', HttpCodes.UNAUTHORIZED));
+    return res.status(HttpCodes.UNAUTHORIZED).json(err('Invalid authorization token', HttpCodes.UNAUTHORIZED));
   }
 };
 
