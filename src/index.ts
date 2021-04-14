d61cb93c21d99c0fee38f63bf3a887b30f910a50
Mert <mertturkmenoglu99@gmail.com>
2021-04-14 14:52:12 +0300

    Update index.ts for routing-controllers

--- src/index.ts
+++ src/index.ts
@@ -7,54 +7,59 @@ import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
 
+import { useContainer, useExpressServer } from 'routing-controllers';
+import { Container } from 'typedi';
 import mongooseOptions from './configs/MongoConfig';
 import morganConfig from './configs/MorganConfig';
 import applicationConfig from './configs/ApplicationConfig';
 
 import Log from './utils/Log';
 import errorHandler from './utils/errorHandler';
-import apiV1Routes from './api/v1/routes';
-import authRoutes from './api/v2/modules/auth/authRoutes';
-import postRoutes from './api/v2/modules/post/postRoutes';
-import userRoutes from './api/v2/modules/user/userRoutes';
-import messageRoutes from './api/v2/modules/message/messageRoutes';
 
 // Load environment variables
 dotenvSafe.config();
 
-const app = express();
+// Read port information
 const PORT = process.env.PORT || applicationConfig.PORT;
 
-const main = async () => {
-  // Connect to MongoDB
-  await mongoose.connect(process.env.MONGO_URI as string, mongooseOptions);
-};
+// Linking TypeDI and routing controllers
+useContainer(Container);
 
-// Application values
+// Initialize server as a standard Express app
+const app = express();
+
+// Set trust proxy value to 1 to enable rate limiting
 app.set('trust proxy', 1);
 
+// Initialize Express middlewares
 app.use(express.json());
 app.use(helmet());
-app.use(cors({
-  exposedHeaders: ['authorization'],
-}));
+app.use(
+  cors({
+    exposedHeaders: ['authorization'],
+  }),
+);
 app.use(morgan(morganConfig));
-
-// Application routes
-app.use('/api/v1', apiV1Routes);
-app.use('/api/v2/auth', authRoutes);
-app.use('/api/v2/post', postRoutes);
-app.use('/api/v2/user', userRoutes);
-app.use('/api/v2/message', messageRoutes);
 
 // Error handler
 app.use(errorHandler);
 
-main().then(() => {
+const main = async () => {
+  // Connect to MongoDB
+  await mongoose.connect(process.env.MONGO_URI as string, mongooseOptions);
+
+  // Initialize routing controllers
+  useExpressServer(app, {
+    controllers: [],
+  });
+
+  // Start listening
   app.listen(PORT, () => {
     Log.i(`Server started on port ${PORT}`);
   });
-}).catch((e) => {
+};
+
+main().catch((e) => {
   console.error(e);
   process.exit(1);
 });
