29e8e188add5fb76147c182fa299b53ec7cff42e
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-29 00:23:02 +0300

    Update auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiTags, ApiConsumes, ApiProduces, ApiCreatedResponse, ApiBadRequestResponse } from '@nestjs/swagger';
+import { BadRequestException, Body, Controller, Get, NotFoundException, Post, UnauthorizedException, Res, UseGuards, Param } from '@nestjs/common';
+import { ApiTags, ApiConsumes, ApiProduces, ApiCreatedResponse, ApiBadRequestResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import { User } from '../user/user.entity';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @ApiTags('auth')
 @ApiConsumes('application/json')
@@ -18,7 +20,7 @@ export class AuthController {
   @Post('register')
   @ApiCreatedResponse({ status: 201, description: 'User registered successfully' })
   @ApiBadRequestResponse({ status: 400 })
-  async register(@Body() dto: RegisterDto): Promise<User> {
+  async register(@Body() dto: RegisterDto): Promise<number> {
     const doesUserExist = await this.authService.doesUserExist(dto.username, dto.email);
 
     if (doesUserExist) {
@@ -26,12 +28,31 @@ export class AuthController {
     }
 
     const savedUser = await this.authService.saveUser(dto);
-    return savedUser;
+    return savedUser.id;
   }
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Body() dto: LoginDto) {
-    return await this.authService.get(dto.email);
+  async login(@Body() dto: LoginDto, @Res() res: Response) {
+    const user = await this.authService.findUserByEmailSelectPassword(dto.email);
+
+    if (!user) {
+      return new NotFoundException(`User not found with email: ${dto.email}`);
+    }
+
+    const doPasswordsMatch = await this.authService.doPasswordsMatch(dto.password, user.password);
+
+    if (!doPasswordsMatch) {
+      return new UnauthorizedException('Email or password is wrong');
+    }
+
+    const bearerToken = await this.authService.getBearerToken(user.id, user.username);
+
+    res.header('authorization', bearerToken);
+    return res.json({
+      id: user.id,
+      username: user.username,
+    });
   }
 
   @Post('password/reset')
@@ -44,8 +65,10 @@ export class AuthController {
     return '';
   }
 
-  @Get()
-  getAuthTest() {
-    return this.authService.get('');
+  @UseGuards(JwtAuthGuard)
+  @Get('/ooo/:email')
+  @ApiBearerAuth()
+  getAuthTest(@Param('email') email: string) {
+    return this.authService.get(email);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '../user/user.entity';
-import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
+import * as argon2 from 'argon2';
+
+import { User } from '../user/user.entity';
 import { RegisterDto } from './dto/register.dto';
+
+export type UserWithoutPassword = Omit<User, 'password'>;
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private readonly usersRepository: Repository<User>
+    private readonly usersRepository: Repository<User>,
+    private jwtService: JwtService,
   ) { }
 
   async doesUserExist(username: string, email: string): Promise<boolean> {
@@ -16,17 +22,52 @@ export class AuthService {
     return user !== undefined;
   }
 
-  async saveUser(dto: RegisterDto): Promise<User> {
+  async saveUser(dto: RegisterDto): Promise<UserWithoutPassword> {
+    const hashedPassword = await argon2.hash(dto.password);
     const savedUser = await this.usersRepository.save({
       email: dto.email,
       name: dto.name,
       username: dto.username,
-      password: dto.password,
+      password: hashedPassword,
     });
     return savedUser;
   }
 
-  async get(email: string): Promise<User | undefined> {
+  async findUserByEmail(email: string): Promise<UserWithoutPassword | undefined> {
+    const user = await this.usersRepository.findOne({ where: [{ email }] });
+    return user;
+  }
+
+  async findUserByEmailSelectPassword(email: string): Promise<User | undefined> {
+    const user = await this.usersRepository.createQueryBuilder('user')
+      .where('user.email = :email')
+      .select('*')
+      .setParameters({ email })
+      .getOne();
+    return user;
+  }
+
+  async doPasswordsMatch(plain: string, hashed: string): Promise<boolean> {
+    const match = await argon2.verify(hashed, plain);
+    return match;
+  }
+
+  async getBearerToken(id: number, username: string): Promise<string> {
+    const payload = {
+      id,
+      username,
+    };
+
+    const jwtToken = this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '7d',
+    });
+
+    const bearerToken = `Bearer ${jwtToken}`;
+    return bearerToken;
+  }
+
+  async get(email: string): Promise<UserWithoutPassword | undefined> {
     return await this.usersRepository.findOne({ where: { email } });
   }
 }
