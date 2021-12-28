227ccb88b2f3971c184a37f8a32f4b4b4bc19339
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-29 00:21:19 +0300

    Add local and JWT strategies

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "../jwt-payload.model";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return {
+      id: payload.id,
+      username: payload.username,
+      email: payload.email,
+      image: payload.image,
+    };
+  }
+}
---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,30 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { User } from 'src/user/user.entity';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string): Promise<User> {
+    const user = await this.authService.findUserByEmailSelectPassword(email);
+
+    if (!user) {
+      console.log('user not found')
+      throw new UnauthorizedException();
+    }
+
+    const match = await this.authService.doPasswordsMatch(password, user.password);
+
+    if (!match) {
+      console.log('password not match')
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
