b6258212065ee84d6f4fa07a99f4a97add479560
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-29 00:18:40 +0300

    Update login dto validation rules

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   @MinLength(1)
+  @IsString()
   email!: string;
 
   @MinLength(8)
+  @IsString()
   password!: string;
 }
