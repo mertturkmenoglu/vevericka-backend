60d71536a0d897d21f3ef8eed2449fe2000c043c
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-31 15:10:16 +0300

    Add auth entity to module import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+import { Auth } from './auth.entity';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -17,6 +19,7 @@ import { LocalStrategy } from './strategies/local.strategy';
         expiresIn: '7d',
       },
     }),
+    TypeOrmModule.forFeature([Auth])
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy]
