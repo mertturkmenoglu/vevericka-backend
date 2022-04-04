1eeba51997416c9bc8d8fc77af3eb9d1b02a46a6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-04 12:28:59 +0300

    Update environment variables example and type definition file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,20 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: string;
-    PORT: string;
-    MONGO_URI: string;
     JWT_SECRET: string;
     SENDGRID_API_KEY: string;
     REDIS_URL: string;
-    POSTGRES_DB: string;
-    POSTGRES_USER: string;
-    POSTGRES_PASSWORD: string;
-    POSTGRES_HOST: string;
     DATABASE_URL: string;
+    PORT: string;
+    CONTENTFUL_SPACE_ID: string;
+    CONTENTFUL_ENVIRONMENT: string;
+    CONTENTFUL_ACCESS_TOKEN: string;
+    CLOUDFLARE_IMAGES_API_TOKEN: string;
+    CLOUDFLARE_IMAGES_ACCOUNT_ID: string;
+    CLOUDFLARE_IMAGES_IMAGE_DELIVERY_BASE_URL: string;
+    CLOUDFLARE_IMAGES_API_BASE_URL: string;
+    BETA_REGISTER_CODE: string;
+    ALGOLIA_APPLICATION_ID: string;
+    ALGOLIA_ADMIN_KEY: string;
   }
 }
