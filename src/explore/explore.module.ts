cffaebc60b402e43cd366cd7c17660d930a9d0a3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-11 20:42:20 +0300

    Add prisma module dependency to Explore Module

--- src/explore/explore.module.ts
+++ src/explore/explore.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { ExploreController } from './explore.controller';
 import { ExploreService } from './explore.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ExploreController],
   providers: [ExploreService],
 })
