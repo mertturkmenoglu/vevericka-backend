06b3b6c2e9bbd195bfcfd7118fbc5c4fb32b5e9e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-14 19:23:43 +0300

    Remove Prisma logging

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,19 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    if (process.env.NODE_ENV !== 'production') {
-      super({
-        log: [
-          { emit: 'stdout', level: 'query' },
-          { emit: 'stdout', level: 'info' },
-          { emit: 'stdout', level: 'warn' },
-          { emit: 'stdout', level: 'error' },
-        ],
-      });
-    }
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
