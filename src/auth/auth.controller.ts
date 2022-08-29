14c26242a6d738ff4c07324b1f311d426adef4e9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-29 21:09:12 +0300

    Update login cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -118,7 +118,6 @@ export class AuthController {
       .cookie('jwt-token', this.authService.signJwtPaylod(payload), {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        domain: process.env.NODE_ENV === 'development' ? 'localhost' : 'vevericka.app',
         maxAge: 1000 * 60 * 60 * 24 * 7,
         path: '/',
       })
